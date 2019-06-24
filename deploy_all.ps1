f9a5b84314e86edd2ac1ca8f8fcd202fe0c73d90
Mallegrissimo <martin.job8@gmail.com>
2019-06-24 15:22:24 +1200

    Update deploy_all.ps1

--- deploy_all.ps1
+++ deploy_all.ps1
@@ -30,33 +30,36 @@ while($confirmation -ne "y")
 #prepare for sql scripts
 $script_params = $config_sql_script_params
 
-Write-Host $($MyInvocation.MyCommand.Name) ": Deploying database level ddl scripts"
-execute_sql_script $database_sql_scripts 	$script_params
-Write-Host $($MyInvocation.MyCommand.Name) ": Deploying database level ddl scripts....Done."
+#5~8 deploy ddl/dml scripts
+	Write-Host $($MyInvocation.MyCommand.Name) ": Deploying database level ddl scripts"
+	execute_sql_script $database_sql_scripts 	$script_params
+	Write-Host $($MyInvocation.MyCommand.Name) ": Deploying database level ddl scripts....Done."
 
-Write-Host $($MyInvocation.MyCommand.Name) ": Deploying application level ddl scripts"
-execute_sql_script $app_sql_scripts 		$script_params
-Write-Host $($MyInvocation.MyCommand.Name) ": Deploying application level ddl scripts....Done."
+	Write-Host $($MyInvocation.MyCommand.Name) ": Deploying application level ddl scripts"
+	execute_sql_script $app_sql_scripts 		$script_params
+	Write-Host $($MyInvocation.MyCommand.Name) ": Deploying application level ddl scripts....Done."
 
-# Create the SSIS Environment and EnvVars
-Write-Host $($MyInvocation.MyCommand.Name) ": Creating environment and Env Vars ssis_folder_name = " $ssis_folder_name "ssis_folder_description = " $ssis_folder_description
-Write-Host $($MyInvocation.MyCommand.Name) ":"$script_params
-Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_CreateEnv.sql" -Variable $script_params 
-create_ssis_env_vars			#This is a function found in deploy_helper_functions.ps1.
 
-# Deploy SSIS package
-$ispac_path=$ispac_filename
-Write-Host $($MyInvocation.MyCommand.Name) ": Deploying SSIS project from " $ispac_path
-Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname  -InputFile $lib_path"helper_deploy_ispac_file.sql" -Variable $script_params 
+#9 deploy sql script to set up SSIS environment and create variables
+	Write-Host $($MyInvocation.MyCommand.Name) ": Creating environment and Env Vars ssis_folder_name = " $ssis_folder_name "ssis_folder_description = " $ssis_folder_description
+	Write-Host $($MyInvocation.MyCommand.Name) ":"$script_params
+	Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_CreateEnv.sql" -Variable $script_params 
+	create_ssis_env_vars			#This is a function found in deploy_helper_functions.ps1.
 
-# Link SSIS EnvVars to to the package params
-Write-Host $($MyInvocation.MyCommand.Name) ": Linking Env Vars to SSIS project/package params"
-Write-Host $($MyInvocation.MyCommand.Name) ": $script_params"
-Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_linkEnvVars.sql" -Variable $script_params
-link_params_to_env_vars			#This is a function found in deploy_helper_functions.ps1
+#10 deploy ISPAC file
+	$ispac_path=$ispac_filename
+	Write-Host $($MyInvocation.MyCommand.Name) ": Deploying SSIS project from " $ispac_path
+	Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname  -InputFile $lib_path"helper_deploy_ispac_file.sql" -Variable $script_params 
 
-# Create agent job
-Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_sql_job.sql"
-Write-Host $($MyInvocation.MyCommand.Name) ": Create Agent job"
-Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"CreateAgentJob.sql" -Variable $script_params
-add_job_steps					#This is a function found in deploy_helper_functions.ps1
+#11 Link SSIS EnvVars to to the package params
+	Write-Host $($MyInvocation.MyCommand.Name) ": Linking Env Vars to SSIS project/package params"
+	Write-Host $($MyInvocation.MyCommand.Name) ": $script_params"
+	Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_linkEnvVars.sql" -Variable $script_params
+	link_params_to_env_vars			#This is a function found in deploy_helper_functions.ps1
+
+#12 Create agent job
+	Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"helper_sql_job.sql"
+	Write-Host $($MyInvocation.MyCommand.Name) ": Create Agent job"
+	Invoke-Sqlcmd -verbose -AbortOnError -ServerInstance $ssis_srvr -Database $ssis_dbname -InputFile $lib_path"CreateAgentJob.sql" -Variable $script_params
+	add_job_steps					#This is a function found in deploy_helper_functions.ps1
+
